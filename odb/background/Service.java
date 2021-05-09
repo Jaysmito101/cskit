007a305d7376d3c568a9f4521d64487d5b1ef542
Jaysmito101 <73700725+Jaysmito101@users.noreply.github.com>
2021-05-10 00:23:10 +0530

    Update Service.java

--- odb/background/Service.java
+++ odb/background/Service.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 public class Service{
-  public void serve(String uid){
+  public static void serve(String uid){
     JOptionPane.showMessageDialog(null, "Outside Code!");
   }
 }
