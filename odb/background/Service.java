bbd01a551bf6f0455da9724b336159acabd525ff
Jaysmito101 <73700725+Jaysmito101@users.noreply.github.com>
2021-05-10 00:13:02 +0530

    Update Service.java

--- odb/background/Service.java
+++ odb/background/Service.java
@@ -1,9 +1,6 @@
+import javax.swing.*;
 public class Service{
   public void serve(String uid){
-    try{
-      Thread.sleep(1000);
-      System.out.println(System.currentTimeMillis());
-      serve(uid);
-    }catch(Exception ex){}
+    JOptionPane.showMessageDialog("Outside Code!");
   }
 }
