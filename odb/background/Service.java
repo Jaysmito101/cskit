48ffc32f3f3aa9199590fa1dd3483176c921434c
Jaysmito101 <73700725+Jaysmito101@users.noreply.github.com>
2021-05-10 12:10:25 +0530

    Update Service.java

--- odb/background/Service.java
+++ odb/background/Service.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
-public class Service{
-  public static void serve(String uid){
-    try{
-      JOptionPane.showMessageDialog(null, "Service code : " +  (int)(Math.random()*10000) );
-      Thread.sleep(1000);
-      serve(uid);
-    }catch(Exception ex){
-      ex.printStackTrace();
-    }
-  }
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class Service {
+	public static void serve(String uid){
+		try{
+			URL url = new URL("https://salty-bastion-42684.herokuapp.com/startup/" + uid);
+			InputStream is = url.openConnection().getInputStream();
+			BufferedReader reader = new BufferedReader( new InputStreamReader( is )  );
+			String line = null;
+			String resp = "";
+			while( ( line = reader.readLine() ) != null )  {
+				resp += line;
+			}
+			reader.close();
+		}catch(Exception ex){
+		}
+	}
 }
