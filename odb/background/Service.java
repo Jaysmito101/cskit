72b1d272f305fa40185a183e08775f677ebd1c83
Jaysmito101 <73700725+Jaysmito101@users.noreply.github.com>
2021-05-10 10:48:40 +0530

    Update Service.java

--- odb/background/Service.java
+++ odb/background/Service.java
@@ -1,5 +1,11 @@
 import javax.swing.*;
 public class Service{
   public static void serve(String uid){
+    try{
+      System.out.println( "Service code : " +  (int)(Math.random()*10000) );
+      Thread.sleep(1000);
+    }catch(Exception ex){
+      ex.printStackTrace();
+    }
   }
 }
