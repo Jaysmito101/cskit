302e0629b912242a865a8b44917d05f7f7037781
Jaysmito101 <73700725+Jaysmito101@users.noreply.github.com>
2021-06-08 01:28:39 +0530

    Update Notifications.java

--- odb/background/Notifications.java
+++ odb/background/Notifications.java
@@ -1,5 +1,5 @@
 public class Notifications{
   public static String generateNotification(String uid){
-    return "No new notifications for now!";
+    return "No new notifications for now!\nSALE - Renew the liscense at just 1$  contact at jaysmito@outlook.com now!!!";
   }
 }
